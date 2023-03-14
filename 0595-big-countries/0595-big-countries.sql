5418ec52c4a8a2286f4505cca2393e66fd7c7a3f
Karim Elsaeed <108232917+karimelsaeedd@users.noreply.github.com>
2023-03-14 23:44:33 +0200

    Time: 608 ms (9.94%), Space: 0B (100.00%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,5 +1,4 @@
-/* Write your T-SQL query statement below */
-
-select name, population, area
-from world
-where area >= 3000000 or population >= 25000000
+# Write your MySQL query statement below
+SELECT name, population, area 
+FROM World
+WHERE area >= 3000000 or population >= 25000000
